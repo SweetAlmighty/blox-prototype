ec290ef8a50fcdff2a556dec57d9556f3f3d338e
Brian Sweet <bsweet48@gmail.com>
2020-11-14 00:15:54 -0500

    BLOX-2
    Added snapping.

--- main.lua
+++ main.lua
@@ -8,6 +8,7 @@ local col_index = { }
 local block_width = 53
 local block_height = 53
 local selected_shape = nil
+local max_int = math.pow(2, 52)
 local colors = {
     { r = 1, g = 0, b = 0 },
     { r = 0, g = 1, b = 0 },
@@ -51,15 +52,23 @@ function create_grid()
         local row = { }
         for j=1, columns, 1 do
             row[#row + 1] = {
-                x = i,
-                y = j,
+                row = i,
+                column = j,
+                checked = false,
+                occupied = false,
+                x = i * block_width,
+                y = j * block_height,
                 neighbors = {
                     { x = i, y = j - 1 > 0 and j - 1 or nil },
                     { x = i - 1 > 0 and i - 1 or nil, y = j },
                     { x = i + 1 <= rows and i + 1 or nil, y = j },
                     { x = i, y = j + 1 <= columns and j + 1 or nil }
-                },
-                checked = false,
+                }
+            }
+
+            row[#row].midpoint = {
+                x = row[#row].x + (block_width / 2),
+                y = row[#row].y + (block_height / 2)
             }
         end
 
@@ -73,7 +82,7 @@ function draw_grid()
     for i=1, #grid, 1 do
         for j=1, #grid[i], 1 do
             local g = grid[i][j]
-            love.graphics.rectangle('fill', g.x * block_width, g.y * block_height, block_width-1, block_height-1)
+            love.graphics.rectangle('fill', g.x, g.y, block_width-1, block_height-1)
         end
     end
 end
@@ -123,16 +132,18 @@ function create_shapes()
 end
 
 function place_shapes()
-    local start = {
-        x = block_width * (rows + 2),
-        y = rows * block_height
-    }
-
     local randomized_shapes = shuffle(shapes)
+    local width, height = love.graphics.getDimensions()
+
     for i=1, #randomized_shapes, 1 do
         local shape = randomized_shapes[i]
+
+        local x = math.random(block_width, width - block_width) - shape[1].x
+        local y = math.random(block_height * 6, height - block_height) - shape[1].y
+
         for j=1, #shape, 1 do
-            shape[j].y = shape[j].y + (start.y + (i * 5))
+            shape[j].x = shape[j].x + x
+            shape[j].y = shape[j].y + y
         end
     end
 end
@@ -164,6 +175,13 @@ function draw_reset_button()
     love.graphics.print('Reset', 51, 2)
 end
 
+function distance(x1,y1,x2,y2)
+    local dx = x1 - x2
+    local dy = y1 - y2
+    local s = dx * dx + dy * dy
+    return squared and s or math.sqrt(s)
+end
+
 function love.draw()
     draw_reset_button()
     draw_grid()
@@ -178,7 +196,7 @@ function love.load(arg)
     place_shapes()
 end
 
-function love.mousemoved(_, _, dx, dy)--, istouch)
+function love.mousemoved(x, y, dx, dy, istouch)
     move_selected_shape(dx, dy)
 end
 
@@ -204,8 +222,60 @@ function love.mousepressed(x, y, button, istouch, presses)
     end
 end
 
-function love.mousereleased()--x, y, button, istouch, presses)
-    -- move shape back towards finger
-    move_selected_shape(0, 100)
-    selected_shape = nil
+function love.mousereleased(x, y, button, istouch, presses)
+    if selected_shape ~= nil then
+        local corners = { }
+        for i=1, #selected_shape, 1 do
+            local min_dist = max_int
+            local closest_gridspace = nil
+
+            for j=1, #grid, 1 do
+                for k=1, #grid[j], 1 do
+                    local midpoint = {
+                        x = selected_shape[i].x + (block_width / 2),
+                        y = selected_shape[i].y + (block_height / 2)
+                    }
+
+                    local dist = distance(grid[j][k].midpoint.x, grid[j][k].midpoint.y, midpoint.x, midpoint.y)
+
+                    if dist < block_height and dist < min_dist then
+                        min_dist = dist
+                        closest_gridspace = {
+                            x = j,
+                            y = k,
+                        }
+                    end
+                end
+            end
+
+            if min_dist ~= max_int then
+                local blah = false
+                for j=1, #corners, 1 do
+                    if not blah and corners[j].x == closest_gridspace.x and corners[j].y == closest_gridspace.y then
+                        blah = true
+                    end
+                end
+
+                if not blah then
+                    corners[#corners + 1] = {
+                        block = i,
+                        x = closest_gridspace.x,
+                        y = closest_gridspace.y
+                    }
+                end
+            end
+        end
+
+        if #corners == #selected_shape then
+            for i=1, #corners, 1 do
+                selected_shape[corners[i].block].x = grid[corners[i].x][corners[i].y].x
+                selected_shape[corners[i].block].y = grid[corners[i].x][corners[i].y].y
+            end
+        else
+            -- move shape back towards finger
+            move_selected_shape(0, 100)
+        end
+
+        selected_shape = nil
+    end
 end
